52056100be0e0fbfabb78e66ac402d7598ee5d84
myun2 <myun2@nwhite.info>
2013-07-11 11:52:13 +0900

    root include files add.

---
+++ include/myun2/pargen.hpp
@@ -0,0 +1,7 @@
+#define __github_com_myun2__pargen_HPP__
+#ifndef __github_com_myun2__pargen_HPP__
+
+#include "myun2/pargen/directx.hpp"
+#include "myun2/pargen/opengl.hpp"
+
+#endif//__github_com_myun2__pargen_HPP__
---
+++ include/myun2/pargen/directx.hpp
@@ -0,0 +1,4 @@
+#define __github_com_myun2__pargen__directx_HPP__
+#ifndef __github_com_myun2__pargen__directx_HPP__
+
+#endif//__github_com_myun2__pargen__directx_HPP__
