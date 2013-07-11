b90e76a560cd9b1ba09c46b4ff40b39ff1c09cf4
myun2 <myun2@nwhite.info>
2013-07-11 14:13:58 +0900

    hlsl/hlsl.hpp

--- include/myun2/pargen/directx/hlsl.hpp
+++ include/myun2/pargen/directx/hlsl.hpp
@@ -1,4 +1,6 @@
 #define __github_com_myun2__pargen__directx__hlsl_HPP__
 #ifndef __github_com_myun2__pargen__directx__hlsl_HPP__
 
+#include "myun2/pargen/directx/hlsl/hlsl.hpp"
+
 #endif//__github_com_myun2__pargen__directx__hlsl_HPP__
