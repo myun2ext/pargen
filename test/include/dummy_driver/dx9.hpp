5ff0f3eaea27fe2a590f488cf5e544bcf62e4006
myun2 <myun2@nwhite.info>
2013-07-11 13:39:33 +0900

    dummy driver dx9.

---
+++ test/include/dummy_driver/dx9.hpp
@@ -0,0 +1,19 @@
+#define __github_com_myun2__pargen__dummy_driver__dx9_HPP__
+#ifndef __github_com_myun2__pargen__dummy_driver__dx9_HPP__
+
+//	Dummy Included
+#define __github_com_myun2__pargen__directx__dx9__includes_HPP__
+
+
+
+#include <windows.h>
+
+struct IDirect3D9 {};
+
+IDirect3D9* Direct3DCreate9(UINT SDKVersion) { return new IDirect3D9; };
+
+
+
+
+
+#endif//__github_com_myun2__pargen__dummy_driver__dx9_HPP__
