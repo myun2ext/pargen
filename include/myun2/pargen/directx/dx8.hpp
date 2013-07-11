0ce84200963e78144b2ff730a87a417dfefe2b6a
myun2 <myun2@nwhite.info>
2013-07-11 12:55:47 +0900

    root dx8, dx9.hpp includes

--- include/myun2/pargen/directx/dx8.hpp
+++ include/myun2/pargen/directx/dx8.hpp
@@ -1,4 +1,6 @@
 #define __github_com_myun2__pargen__directx__dx8_HPP__
 #ifndef __github_com_myun2__pargen__directx__dx8_HPP__
 
+#include "myun2/pargen/directx/dx8/d3d.hpp"
+
 #endif//__github_com_myun2__pargen__directx__dx8_HPP__
--- include/myun2/pargen/directx/dx9.hpp
+++ include/myun2/pargen/directx/dx9.hpp
@@ -1,4 +1,6 @@
 #define __github_com_myun2__pargen__directx__dx9_HPP__
 #ifndef __github_com_myun2__pargen__directx__dx9_HPP__
 
+#include "myun2/pargen/directx/dx9/d3d.hpp"
+
 #endif//__github_com_myun2__pargen__directx__dx9_HPP__
