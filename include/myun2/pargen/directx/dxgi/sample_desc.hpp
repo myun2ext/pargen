8d743b83b01271a28a3282a06971462ac79b3a7b
myun2 <myun2@nwhite.info>
2013-07-11 20:35:33 +0900

    dxgi/sample_desc.hpp Add

---
+++ include/myun2/pargen/directx/dxgi/sample_desc.hpp
@@ -0,0 +1,25 @@
+#define __github_com_myun2__pargen__directx__dxgi__sample_desc_HPP__
+#ifndef __github_com_myun2__pargen__directx__dxgi__sample_desc_HPP__
+
+#include <DXGI.h>
+
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace directx
+		{
+			namespace dxgi
+			{
+				class sample_desc
+				{
+					//	DXGI_SAMPLE_DESC 
+					//    UINT Count;
+					//    UINT Quality;
+				};
+			}
+		}
+	}
+}
+
+#endif//__github_com_myun2__pargen__directx__dxgi__sample_desc_HPP__
