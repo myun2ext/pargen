baee078fcc7add48e2427e1fa5effa5ebaf474e0
myun2 <myun2@nwhite.info>
2013-07-11 12:07:10 +0900

    dx9/includes.hpp Add

---
+++ include/myun2/pargen/directx/dx9/includes.hpp
@@ -0,0 +1,10 @@
+#define __github_com_myun2__pargen__directx__dx9__includes_HPP__
+#define __github_com_myun2__pargen__directx__dx9__includes_HPP__
+
+#include <d3d9.h>
+#include <d3dx9core.h>
+#include <d3dx9effect.h>
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+#endif//__github_com_myun2__pargen__directx__dx9__includes_HPP__
