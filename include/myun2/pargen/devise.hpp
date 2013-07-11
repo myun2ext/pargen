a90443ebe162e4be4a02e6aab6623863f61a32d9
myun2 <myun2@nwhite.info>
2013-07-11 16:22:41 +0900

    namespace Add.

--- include/myun2/pargen/devise.hpp
+++ include/myun2/pargen/devise.hpp
@@ -1,4 +1,11 @@
 #define __github_com_myun2__pargen__driver_HPP__
 #ifndef __github_com_myun2__pargen__driver_HPP__
 
+namespace myun2
+{
+	namespace pargen
+	{
+	}
+}
+
 #endif//__github_com_myun2__pargen__driver_HPP__
--- include/myun2/pargen/texture.hpp
+++ include/myun2/pargen/texture.hpp
@@ -1,4 +1,11 @@
 #define __github_com_myun2__pargen__texture_HPP__
 #ifndef __github_com_myun2__pargen__texture_HPP__
 
+namespace myun2
+{
+	namespace pargen
+	{
+	}
+}
+
 #endif//__github_com_myun2__pargen__texture_HPP__
