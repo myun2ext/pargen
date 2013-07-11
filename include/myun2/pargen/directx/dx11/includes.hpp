1103fc7efcd9e6e1386c4cff758e71fc38bd656e
myun2 <myun2@nwhite.info>
2013-07-11 14:09:56 +0900

    dx11/includes.hpp ...? (un-tested)

---
+++ include/myun2/pargen/directx/dx11/includes.hpp
@@ -0,0 +1,19 @@
+#define __github_com_myun2__pargen__directx__dx11__includes_HPP__
+#ifndef __github_com_myun2__pargen__directx__dx11__includes_HPP__
+
+#include <windows.h>
+#include <tchar.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <dxerr.h>
+
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "d3d11.lib")
+#if _DEBUG
+  #pragma comment(lib, "d3dx11d.lib")
+#else
+  #pragma comment(lib, "d3dx11.lib")
+#endif
+
+
+#endif//__github_com_myun2__pargen__directx__dx11__includes_HPP__
