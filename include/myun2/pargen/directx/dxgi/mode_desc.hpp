1c36df9251ef018bc55ab3e6c489b216da51b3bd
myun2 <myun2@nwhite.info>
2013-07-11 20:30:15 +0900

    dxgi/swap_chain_desc.hpp Add
    dxgi/mode_desc.hpp Add

---
+++ include/myun2/pargen/directx/dxgi/mode_desc.hpp
@@ -0,0 +1,23 @@
+#define __github_com_myun2__pargen__directx__dxgi__mode_desc_HPP__
+#ifndef __github_com_myun2__pargen__directx__dxgi__mode_desc_HPP__
+
+#include <DXGI.h>
+
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace directx
+		{
+			namespace dxgi
+			{
+				class mode_desc
+				{
+					//	DXGI_MODE_DESC
+				};
+			}
+		}
+	}
+}
+
+#endif//__github_com_myun2__pargen__directx__dxgi__mode_desc_HPP__
---
+++ include/myun2/pargen/directx/dxgi/swap_chain_desc.hpp
@@ -0,0 +1,23 @@
+#define __github_com_myun2__pargen__directx__dxgi__swap_chain_desc_HPP__
+#ifndef __github_com_myun2__pargen__directx__dxgi__swap_chain_desc_HPP__
+
+#include <DXGI.h>
+
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace directx
+		{
+			namespace dxgi
+			{
+				class swap_chain_desc
+				{
+					//	DXGI_SWAP_CHAIN_DESC
+				};
+			}
+		}
+	}
+}
+
+#endif//__github_com_myun2__pargen__directx__dxgi__swap_chain_desc_HPP__
