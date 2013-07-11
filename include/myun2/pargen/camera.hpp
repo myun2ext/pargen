aa6c0555fe71f624f9e88bbf531e4a92b77dccad
myun2 <myun2@nwhite.info>
2013-07-11 19:44:38 +0900

    pargen: camera.hpp, fog.hpp, particle.hpp, world.hpp

---
+++ include/myun2/pargen/camera.hpp
@@ -0,0 +1,11 @@
+#define __github_com_myun2__pargen__camera_HPP__
+#ifndef __github_com_myun2__pargen__camera_HPP__
+
+namespace myun2
+{
+	namespace pargen
+	{
+	}
+}
+
+#endif//__github_com_myun2__pargen__camera_HPP__
