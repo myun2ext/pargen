85568bfcb439914ecc98eb9e12650f67cc597643
myun2 <myun2@nwhite.info>
2013-07-11 12:22:30 +0900

    directx.hpp namespacing.

--- include/myun2/pargen/directx/directx.hpp
+++ include/myun2/pargen/directx/directx.hpp
@@ -1,4 +1,14 @@
 #define __github_com_myun2__pargen__directx__direct_HPP__
 #ifndef __github_com_myun2__pargen__directx__direct_HPP__
 
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace directx
+		{
+		}
+	}
+}
+
 #endif//__github_com_myun2__pargen__directx__direct_HPP__
