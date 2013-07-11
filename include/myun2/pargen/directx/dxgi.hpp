4cfe996e18a99a86ff7acebee15b880b4c7a40bd
myun2 <myun2@nwhite.info>
2013-07-11 20:37:10 +0900

    dxgi.hpp Add

---
+++ include/myun2/pargen/directx/dxgi.hpp
@@ -0,0 +1,6 @@
+#define __github_com_myun2__pargen__directx__dxgi_HPP__
+#ifndef __github_com_myun2__pargen__directx__dxgi_HPP__
+
+#include "myun2/pargen/directx/dxgi/dxgi.hpp"
+
+#endif//__github_com_myun2__pargen__directx__dxgi_HPP__
---
+++ include/myun2/pargen/directx/dxgi/dxgi.hpp
@@ -0,0 +1,8 @@
+#define __github_com_myun2__pargen__directx__dxgi__dxgi_HPP__
+#ifndef __github_com_myun2__pargen__directx__dxgi__dxgi_HPP__
+
+#include "myun2/pargen/directx/dxgi/mode_desc.hpp"
+#include "myun2/pargen/directx/dxgi/sample_desc.hpp"
+#include "myun2/pargen/directx/dxgi/swap_chain_desc.hpp"
+
+#endif//__github_com_myun2__pargen__directx__dxgi__dxgi_HPP__
