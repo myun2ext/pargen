c95ead8540aeac86882947e5509e53b97fc3a916
myun2 <myun2@nwhite.info>
2013-07-11 14:07:42 +0900

    dx10/includes.hpp Add

---
+++ include/myun2/pargen/directx/dx10/includes.hpp
@@ -0,0 +1,19 @@
+#define __github_com_myun2__pargen__directx__dx10__includes_HPP__
+#ifndef __github_com_myun2__pargen__directx__dx10__includes_HPP__
+
+#include <windows.h>
+#include <tchar.h>
+#include <d3d10.h>
+#include <d3dx10.h>
+#include <dxerr.h>
+
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "d3d10.lib")
+#if _DEBUG
+  #pragma comment(lib, "d3dx10d.lib")
+#else
+  #pragma comment(lib, "d3dx10.lib")
+#endif
+
+
+#endif//__github_com_myun2__pargen__directx__dx10__includes_HPP__
