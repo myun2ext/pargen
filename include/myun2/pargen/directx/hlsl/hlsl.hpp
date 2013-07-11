c15d252797dfb301a11cd6d2d30dfa5892f8a641
myun2 <myun2@nwhite.info>
2013-07-11 16:10:21 +0900

    hlsl.hpp: namespace

--- include/myun2/pargen/directx/hlsl/hlsl.hpp
+++ include/myun2/pargen/directx/hlsl/hlsl.hpp
@@ -1,4 +1,16 @@
 #define __github_com_myun2__pargen__directx__hlsl__hlsl_HPP__
 #ifndef __github_com_myun2__pargen__directx__hlsl__hlsl_HPP__
 
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace hlsl
+		{
+		}
+	}
+}
+
+
+
 #endif//__github_com_myun2__pargen__directx__hlsl__hlsl_HPP__
