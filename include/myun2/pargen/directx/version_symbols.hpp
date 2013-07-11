c7d6fd5c53648ecdfe8b0088a5d1f047dcecc608
myun2 <myun2@nwhite.info>
2013-07-11 14:13:02 +0900

    version_symbols.hpp Add

---
+++ include/myun2/pargen/directx/version_symbols.hpp
@@ -0,0 +1,18 @@
+#define __github_com_myun2__pargen__directx__version_symbols_HPP__
+#ifndef __github_com_myun2__pargen__directx__version_symbols_HPP__
+
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace directx
+		{
+			class dx8;
+			class dx9;
+			class dx10;
+			class dx11;
+		}
+	}
+}
+
+#endif//__github_com_myun2__pargen__directx__version_symbols_HPP__
