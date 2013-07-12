0dac27bfa6d2fb1831d2e2ae8d86c61e623fdb6d
myun2 <myun2@nwhite.info>
2013-07-12 12:14:25 +0900

    opengl/opengl.hpp

---
+++ include/myun2/pargen/opengl/opengl.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__pargen__opengl__opengl_HPP__
+#define __github_com_myun2__pargen__opengl__opengl_HPP__
+
+namespace myun2
+{
+	namespace pargen
+	{
+		namespace opengl
+		{
+		}
+	}
+}
+
+#endif//__github_com_myun2__pargen__opengl__opengl_HPP__
