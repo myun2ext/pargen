d6a62d74bd1c25daca7798760bca22b51efaee82
myun2 <myun2@nwhite.info>
2013-07-12 16:36:33 +0900

    texture.hpp Update

--- include/myun2/pargen/texture.hpp
+++ include/myun2/pargen/texture.hpp
@@ -1,12 +1,21 @@
 #ifndef __github_com_myun2__pargen__texture_HPP__
 #define __github_com_myun2__pargen__texture_HPP__
 
+#include <string>
+
 namespace myun2
 {
 	namespace pargen
 	{
 		class texture
 		{
+		public:
+			texture() { }
+			texture(const char* filename) { load(filename); }
+			texture(const ::std::string& filename) { load(filename); }
+
+			void load(const char* filename) { }
+			void load(const ::std::string& filename) { load(filename.c_str(); }
 		};
 	}
 }
