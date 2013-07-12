81ce8326f3a5c67fa2de353e819a5e4a399e9b4b
myun2 <myun2@nwhite.info>
2013-07-12 12:37:38 +0900

    myun2/pargen.hpp include myun2/pargen/pargen.hpp (not directx / opengl, user selectable...)

--- include/myun2/pargen.hpp
+++ include/myun2/pargen.hpp
@@ -1,7 +1,6 @@
 #ifndef __github_com_myun2__pargen_HPP__
 #define __github_com_myun2__pargen_HPP__
 
-#include "myun2/pargen/directx.hpp"
-#include "myun2/pargen/opengl.hpp"
+#include "myun2/pargen/pargen.hpp"
 
 #endif//__github_com_myun2__pargen_HPP__
