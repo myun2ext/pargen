8e4cadfc47c108e3b0d27e1eef08dc0ac4e00b5c
myun2 <myun2@nwhite.info>
2013-07-12 11:56:45 +0900

    pargen/pargen.hpp Add

---
+++ include/myun2/pargen/pargen.hpp
@@ -0,0 +1,16 @@
+#ifndef __github_com_myun2__pargen__pargen_HPP__
+#define __github_com_myun2__pargen__pargen_HPP__
+
+#include "myun2/pargen/devise.hpp"
+
+#include "myun2/pargen/fog.hpp"
+#include "myun2/pargen/blur.hpp"
+#include "myun2/pargen/camera.hpp"
+#include "myun2/pargen/lighting.hpp"
+#include "myun2/pargen/particle.hpp"
+#include "myun2/pargen/shadow.hpp"
+#include "myun2/pargen/shape.hpp"
+#include "myun2/pargen/texture.hpp"
+#include "myun2/pargen/world.hpp"
+
+#endif//__github_com_myun2__pargen__pargen_HPP__
