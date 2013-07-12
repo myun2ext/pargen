8fbc2c6b2e6b682d2bebd5d1a044ffb75882fd96
myun2 <myun2@nwhite.info>
2013-07-12 12:37:59 +0900

    opengl test

--- test/opengl/001.cpp
+++ test/opengl/001.cpp
@@ -3,6 +3,19 @@
 
 using namespace myun2::pargen::opengl;
 
-int main() {
+void disp(void) {
+	glClearColor(1 , 1 , 1 , 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc , char ** argv) {
+	glutInit(&argc , argv);
+	glutInitWindowPosition(100 , 50);
+	glutInitWindowSize(200 , 100);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+
+	glutCreateWindow("Kitty on your lap");
+	glutDisplayFunc(disp);
+	glutMainLoop();
 	return 0;
 }
--- test/opengl/make.bat
+++ test/opengl/make.bat
@@ -1,12 +1,13 @@
 @echo off
 set CC=mingw32-c++
-set INCLUDES=../../include
+set INCLUDES=../../include -I C:\Applications\glut-3.5\include
+set LINKS=-L C:\Applications\glutdlls -lglut  -lopengl32 -lglu32 -lgdi32
 set CFLAGS=-O3
 set SOURCES=001.cpp
 set OUTFILE=001.exe
 
 @echo on
-%CC% -I %INCLUDES% %CFLAGS% %SOURCES% -o %OUTFILE%
+%CC% -I %INCLUDES% %CFLAGS% %SOURCES% %LINKS% -o %OUTFILE%
 
 %OUTFILE%
 
