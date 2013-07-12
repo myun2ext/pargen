59fedd8a695afed55a7393f9d8e33349a3f32f2a
myun2 <myun2@nwhite.info>
2013-07-12 12:19:50 +0900

    opengl includes,
    opengl test

---
+++ include/myun2/pargen/opengl/includes.hpp
@@ -0,0 +1,8 @@
+#ifndef __github_com_myun2__pargen__opengl__includes_HPP__
+#define __github_com_myun2__pargen__opengl__includes_HPP__
+
+#include <windows.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+#endif//__github_com_myun2__pargen__opengl__includes_HPP__
---
+++ test/opengl/001.cpp
@@ -0,0 +1,8 @@
+#include "myun2/pargen.hpp"
+#include "myun2/pargen/opengl/opengl.hpp"
+
+using namespace myun2::pargen::opengl;
+
+int main() {
+	return 0;
+}
