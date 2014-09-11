9c594e806873cc7de4a9734fd91888b08dd2e6d7
myun2 <myun2@nwhite.info>
2014-09-11 16:32:14 +0900

    window.hpp add

---
+++ include/myun2/pargen/window.hpp
@@ -0,0 +1,6 @@
+#ifndef __github_com_myun2__pargen__window_HPP__
+#define __github_com_myun2__pargen__window_HPP__
+
+#include "myun2/pargen/window/window.hpp"
+
+#endif//__github_com_myun2__pargen__window_HPP__
